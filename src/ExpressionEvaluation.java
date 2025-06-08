dd7d77548b81a883eaf1fb89d3b35f7ace2501b2
vaibhav_kanthak <vaibhavkanthak@gmail.com>
2025-06-08 22:04:44 +0530

    basic programs june 08

---
+++ src/ExpressionEvaluation.java
@@ -0,0 +1,8 @@
+//Write a Java program to compute the specified expressions and print the output.
+public class ExpressionEvaluation {
+
+    public static void main(String[] args) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
